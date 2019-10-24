466b53150c0ea960c240ca33be70c7e01921dab8
Brett Fodor <bmattmusic@gmail.com>
2019-10-24 15:43:42 -0400

    blackjack logic imported

--- webpack-blackjack/src/js/main.js
+++ webpack-blackjack/src/js/main.js
@@ -1,3 +1,12 @@
+import Blackjack from "blackjack-dealer-logic"
+
+
+
 export default () => {
     alert('it works')
+
+    const app = document.getElementById('app')
+    const blackjack = new Blackjack(app)
+    blackjack.start()
+    
 }
